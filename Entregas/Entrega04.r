f2f0c90d4831966bf97ab3443e1f645e09262370
davidgc14 <curbelo.gc@gmail.com>
2022-03-24 08:52:15 +0100

    Inicialización de la entrega día 24

---
+++ Entregas/Entrega04.r
@@ -0,0 +1,6 @@
+#############################################
+############### EJERCICIO 1 #################
+#############################################
+
+# Apartado a)
+censo <- read.csv("Census.csv", header=TRUE) # Tenemos encabezado y separador de coma
