96f4dc5b94a00c8f90a1dbc8067d8f17259cc992
davidgc14 <curbelo.gc@gmail.com>
2022-04-26 09:26:53 +0200

    Renombre de archivo

---
+++ Apuntes/Tema 4: Gráficos/0_plot.r
@@ -0,0 +1,15 @@
+##############################
+########## PLOT ##############
+##############################
+
+plot(cars$speed, cars$dist) # plot(vectorX, vectorY)
+
+head(cars) # información de los datos. Distancia de frenado en función de la velocidad
+
+
+# Hay tabla de parámetros de la función en pagina 5 tema 4
+plot(cars$speed, cars$dist, type="l") # ejemplo
+
+
+
+
