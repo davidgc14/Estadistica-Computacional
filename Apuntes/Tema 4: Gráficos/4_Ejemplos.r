815220f321f2b35aa2cf9824746c73445a4dff3a
davidgc14 <curbelo.gc@gmail.com>
2022-05-03 10:55:23 +0200

    Fin del ejemplo

--- Apuntes/Tema 4: Gráficos/4_Ejemplos.r
+++ Apuntes/Tema 4: Gráficos/4_Ejemplos.r
@@ -20,3 +20,19 @@ summary(fit) # Observamos R² que nos indica lo bien que están ajustados los da
 # Realiza un ejemplo de regresion lineal e imprime el grafico
 
 anova(fit) # Anova proporciona los cálculos intermedios
+
+# Estudios de coordenadas y pendientes
+confint(fit)
+
+# Realización de predicciones con el modelo
+predict(fit, newdata =data.frame(bp=200), interval='confidence', se.fit=TRUE) # predicción de la presión para una temperatura de 0 y 100
+
+
+# Impresión del error de predicción y de precisión
+
+x0 <- data.frame(bp=seq(min(forbes$bp), max(forbes$bp), length.out=20))
+pred.m <- predict(fit, newdata=x0, interval='confidence', se.fit=T)
+pred.p <- predict(fit, newdata=x0, interval='prediction', se.fit=T)
+matplot(x0$bp, cbind(pred.m$fit, pred.p$fit[, -1]), lty=c(1, 2, 2, 3, 3), col=c(1, 2, 2, 4, 4), type='l', xlab='temperatura', ylab='presión', main='')
+legend('topleft', c('Media', 'Predicción'),  lty=c(2, 3), col=c(2, 4), bt='n')
+points(forbes$bp, forbes$pres)
