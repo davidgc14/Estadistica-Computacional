592e614e3a47214099bfde04d3a05813f36256b4
davidgc14 <curbelo.gc@gmail.com>
2022-05-10 10:15:43 +0200

    Algunas notas básicas de Rmd

--- Apuntes/Tema 4: Gráficos/5_InformesEstadisticos.r
+++ Apuntes/Tema 4: Gráficos/5_InformesEstadisticos.r
@@ -8,7 +8,17 @@ render("prueba.Rmd")
 # Se crea un fichero intermedio que despues se elimina
 # Para que no borre este archivo introducimos lo siguiente
 
-render("prueba.Rmd", clean = FALSE)
+render("prueba.Rmd", clean = FALSE) # no es necesario
 
 # Para abrir el documento html en el buscador
 browseURL(url=render('ejemplo1.Rmd'))
+
+# Las ediciones en el documento se actualizan sobre la marcha en el
+# documento preview
+
+# Al indicar include=FALSE, se evita que aparezca en el documento
+```{r setup, include=FALSE}
+
+
+# Empezar con ``` es equivalente a $$
+# Si usamos uno solo es equivalente a $ 
