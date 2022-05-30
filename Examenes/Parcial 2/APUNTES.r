032b2e7b8e0a29fa4b01728f83df588b24287ab9
davidgc14 <curbelo.gc@gmail.com>
2022-05-30 13:24:37 +0200

    Fin de práctica 8

--- Examenes/Parcial 2/APUNTES.r
+++ Examenes/Parcial 2/APUNTES.r
@@ -343,10 +343,51 @@ rs <- rstandard(fit)
 plot(fit$fitted.values, rstandard(fit))
 abline(h = 0, col = 'red')
 
-# Test de Durbin-Watson (Para linealidad)
+# Test de Durbin-Watson (Para incorrelación)
 library(lmtest)
 dwtest(fit)
 
 # Test de Kolmogorov-Smirnov (Para normalidad)
 ks.test(rs, pnorm) # p-valor alto = normal
 qqnorm(rs) # comprobación gráfica de la conclusión
+
+# Gráficos de linealidad
+library(car)
+crPlots(fit)
+
+
+
+###############################################
+################ DATOS ANÓMALOS ###############
+###############################################
+
+# Detección de dtos con errores muy grandes
+which(abs(rs)>2) # Devuelve empresas anómalas
+
+# Medición de la influencia
+cooks.distance(fit) # Influencia de cada variable
+
+# Medición de aislamiento
+hatvalues(fit)
+
+# Observación de datos de aislamiento y anomalía en gráfica
+influenceIndexPlot(fit) # influencia, residuo, anomalía y aislamiento
+
+# Eliminación de datos anómalos
+hatco <- hatco[-c(7,100),] # Eliminamos las empresas 7 y 100
+fit2 <- lm(fidelidad ~ velocidad+precio+flexprec+imgfabri+servconj+imgfvent+calidadp, hatco)
+
+
+
+###############################################
+########## SELEC. VAR. EXPLICATIVAS ###########
+###############################################
+
+# Multicolinealidad: variables redundantes. Confirmación de si hay
+R <- cor(hatco[,6:12]) # No deben haber dats elevados
+ai <- eigen(R)$values ; sqrt(max(ai)/min(ai)) # Índice de condicionamiento
+vif(fit2) # factor de inflado de varianza
+
+# Eliminación de variables no explicativas
+step(fit2) # Forward
+step(fit2, direction = 'both') # ida y vuelta
